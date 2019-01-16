34ad9383d4051c04b70261b87bca6d63e1231fd7
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-16 12:04:03 -0800

    enum for suits created

---
+++ Collections/Collections/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class Deck<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
     class Program
     {
+        enum Suits { Hearts, Diamonds, Spades, Clubs }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
